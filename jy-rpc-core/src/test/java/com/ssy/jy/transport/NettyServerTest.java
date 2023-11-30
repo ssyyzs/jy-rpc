4d1ec0f886998bbdd1c6c96fb4a6116c1290f79d
y30010171 <ssyyzs1314@163.com>
2023-11-30 15:00:45 +0800

    resolve error

--- jy-rpc-core/src/test/java/com/ssy/jy/transport/NettyServerTest.java
+++ jy-rpc-core/src/test/java/com/ssy/jy/transport/NettyServerTest.java
@@ -19,20 +19,4 @@ class NettyServerTest {
             System.out.println(resources.nextElement().getFile());
         }
     }
-H
-    public int[] productExceptSelf(int[] nums) {
-        int[] pre = new int[nums.length + 1];
-        int[] suf = new int[nums.length + 1];
-        pre[0] = 1;
-        suf[nums.length] = 1;
-        for (int i = 0; i < nums.length; i++) {
-            pre[i + 1] = pre[i] * nums[i];
-            suf[nums.length - 1 - i] = suf[nums.length - i] * nums[nums.length - 1 - i];
-        }
-        int[] ans = new int[nums.length];
-        for (int i = 0; i < ans.length; i++) {
-            ans[i] = suf[i + 1] * pre[i];
-        }
-        return ans;
-    }
 }
